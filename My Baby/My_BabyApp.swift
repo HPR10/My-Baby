13f972d514373c013c8eb10c75dc0826b36cbe47
Hugo Pinheiro <hugo_hp10@hotmail.com>
2022-01-08 16:06:21 -0300

    Initial Commit

---
+++ My Baby/My_BabyApp.swift
@@ -0,0 +1,17 @@
+//
+//  My_BabyApp.swift
+//  My Baby
+//
+//  Created by Hugo Pinheiro on 08/01/22.
+//
+
+import SwiftUI
+
+@main
+struct My_BabyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
